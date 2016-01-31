1afde02d3eeb48f3365a3c1d84cc6c0941a2a5d2
Thuong Ho <thuong.t.ho@gmail.com>
2016-01-30 20:41:05 -0800

    add item completion feature

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,22 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
-
+.controller('listCtrl', function($scope, $ionicListDelegate) {
+  $scope.items = [];
+  
+  $scope.addItem = function() {
+    var name = prompt('What do you want to add to the list?');
+    if (name) {
+      $scope.items.push({'name': name});
+    }
+  };
+  
+  $scope.completedTask = function(item) {
+    $scope.item = item;
+    $scope.item['status'] = 'completed';
+    $ionicListDelegate.closeOptionButtons();
+  };
+})
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
